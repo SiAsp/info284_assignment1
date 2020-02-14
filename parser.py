74ce8c4ffabc5ba33c700f0db646e5fabef0b65b
Sindre Asplem <asplem@pc-197-255.eduroam.uib.no>
2020-02-14 12:10:34 +0100

    Add compile_word_bunch for word histogram

--- parser.py
+++ parser.py
@@ -1,41 +1,52 @@
 import csv
+import operator
 import re
 
 
 def parse(f):
     with open(f) as csv_content:
         content = list(csv.reader(csv_content, delimiter=','))
-        # Format: ['airline_sentiment', 'airline_sentiment_confidence', 'negativereason', 'negativereason_confidence', 'text']
-        tweets = []
+        # Format: ['airline_sentiment', 'text']
+        tweet_list = []
         for tweet in content[1:]:
             current = []
-            [current.append(tweet[i]) for i in range(1, 5)]
+            current.append(tweet[1])
             text = check_negation(tweet[10])
             current.append(text)
-            tweets.append(current)
-
-        return tweets,
+            tweet_list.append(current)
+        return tweet_list
 
 
 def check_negation(text):
     # Splits text on delimiters and adds 'NOT_' to words (that are not in negatives) in part of text containing negative word.
-    negatives = ["n't", 'not', 'never', 'neither', 'nor', 'nothing']
-    delimiters = [',', '.', ';', ':', '!', '?']
+    negatives = ["n't", 'no', 'not', 'neither', 'nor']
+    delimiters = ',|.|...|;|:|!|?'
     # If a negatory word in text
     if any(neg in text for neg in negatives):
         # Split on delimiters
-        regexPattern = '|'.join(map(re.escape, delimiters))
-        text = re.split(regexPattern, text)
+        text = text.split(delimiters)
         new_text = ''
         for partial in text:
             # If a negatory word in partial text
             if any(neg in partial for neg in negatives):
                 # Add NOT_ to words except words in negatives
-                partial = ' '.join(
-                    ['NOT_' + word if word not in negatives else 'NOT_' + word if word[-3:] != "n't" else word for word in partial.split()])
+                partial = ' '.join([word if word in negatives else
+                                    word if word[-3:] == "n't" else 'NOT_' + word for word in partial.split()])
             new_text += partial + '. '
         return new_text
     return text
+
+
+def compile_word_bunch(tweet_list):
+    delimiters = ',|.|...|;|:|!|?|'
+    word_count = {'positive': {}, 'negative': {}, 'neutral': {}}
+    for sentiment, text in tweet_list:
+        for word in text.split():
+            word = re.sub('[\.|\?|!||(|)|~"]+', '', word.lower())
+            #word = re.sub('[^A-Za-z0-9-_@]+', '', word)
+            word_count[sentiment].setdefault(word, 0)
+            word_count[sentiment][word] += 1
+    return word_count
 
 
 class NaiveBayesClassifier():
@@ -44,8 +55,13 @@ class NaiveBayesClassifier():
 
 
 def main():
-    tweet_list = parse(
-        './data.csv')
+    file = './data.csv'
+    tweet_list = parse(file)
+    bunch = compile_word_bunch(tweet_list)
+    for sentiment, val in bunch.items():
+        sorted_dct = sorted(val.items(), key=operator.itemgetter(1))
+        print(sorted_dct)
+# print(tweet_list)
 
 
 if __name__ == "__main__":
