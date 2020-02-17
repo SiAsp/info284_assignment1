2e6ccb8cfe55a0118fa626df39292a65c3236217
Sindre Asplem <asplem@Sindres-MacBook-Pro.local>
2020-02-17 10:23:44 +0100

    Implement spacy stopwords, add sentiment140 data to repo.

--- parser.py
+++ parser.py
@@ -1,18 +1,22 @@
 import csv
 import operator
 import re
+import spacy
+
 from collections import Counter
-from time import time
-from math import log
 from sklearn.model_selection import train_test_split
+from time import time
 
 class NaiveBayesClassifier():
     '''
     ML classifier using Naive Bayes Classifier algoritm.
     '''
     def __init__(self):
+        spacy_nlp = spacy.load('en_core_web_sm')
+        print(f'spaCy Version: {spacy.__version__}')
+
+        self.stop_words = spacy.lang.en.stop_words.STOP_WORDS
         self.classes = ['positive', 'negative', 'neutral']
-        self.stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
     def parse(self, f, delimiter=',', class_index=1, confidence_index=2, text_index=10, use_confidence=True, negate=False, sentiment140=False):
         '''
